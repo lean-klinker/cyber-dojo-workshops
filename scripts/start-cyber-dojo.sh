a7d2e0367fa004ccf6999c6835930f18101e4998
Bryce Klinker <bklinker@leantechniques.com>
2020-05-21 14:10:01 -0500

    Updating start script to start with the correct custom starting points

--- scripts/start-cyber-dojo.sh
+++ scripts/start-cyber-dojo.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+stop() {
+    ${CYBER_DOJO_SCRIPT_PATH} down
+}
+
 
 start() {
-    ${CYBER_DOJO_SCRIPT_PATH} up
+    ${CYBER_DOJO_SCRIPT_PATH} up --custom=${CYBER_DOJO_CUSTOM_START_POINT_NAME} 
 }
 
 main() {
+    stop
     start
 }
 
