09f97823e9f59429237e693059afce0d45b7a15f
Bryce Klinker <bklinker@leantechniques.com>
2020-05-22 10:10:27 -0500

    adding setup to start script

--- scripts/start-cyber-dojo.sh
+++ scripts/start-cyber-dojo.sh
@@ -6,12 +6,17 @@ stop() {
     ${CYBER_DOJO_SCRIPT_PATH} down
 }
 
+setup() {
+    /bin/bash ./scripts/setup-cyber-dojo.sh
+}
+
 start() {
     ${CYBER_DOJO_SCRIPT_PATH} up --custom=${CYBER_DOJO_CUSTOM_START_POINT_NAME} 
 }
 
 main() {
     stop
+    setup
     start
 }
 
