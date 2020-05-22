6ca5fe929e5f8fb0c91304e2d2bdeedc729025b8
Bryce Klinker <bklinker@leantechniques.com>
2020-05-22 14:28:52 -0500

    Updating to use the correct configuration values

--- intro-to-tdd/javascript/cyber-dojo.sh
+++ intro-to-tdd/javascript/cyber-dojo.sh
@@ -4,10 +4,4 @@ set -e
 
 NODE_VERSION=14.1.0
 
-n use ${NODE_VERSION}
-
-npm-install-offline install jest @babel/core @babel/preset-env \
-    --repo /usr/local/lib/node_modules \
-    --local-only
-
-npm test
+n use ${NODE_VERSION} /usr/local/lib/node_modules/jest/bin/jest --ci
