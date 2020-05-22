28fc1f361a32b19661ede3d4f16d8e57d66e7718
Bryce Klinker <bklinker@leantechniques.com>
2020-05-22 09:33:46 -0500

    Adding script to build container images

---
+++ scripts/build-docker-images.sh
@@ -0,0 +1,12 @@
+build_image() {
+    image_name=${1}
+    docker_file_path=${2}
+
+    docker build -t "${image_name}:latest" ${docker_file_path}
+}
+
+main() {
+    build_image "leanklinker/cyber-dojo-javascript-jest" "./languages/javascript/jest"
+}
+
+main
